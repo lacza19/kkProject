f8411cf5db3edb5510d5fa79ce8c7217cc264c10
varglaszlo <va.laszlo91@gmail.com>
2023-05-04 17:17:21 +0200

    kkProject 0.1

---
+++ src/main/java/com/example/kkproject/KkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kkproject/KkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
